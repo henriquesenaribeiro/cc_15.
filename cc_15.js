f3679059c607bd5c69ec5298adf2a497115f6dad
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-03-15 19:53:42 -0300

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,8 @@
+// Task 1 - Base Structure Setup
+
+document.addEventListener("DOMContentLoaded", () => {
+    const riskDashboard = document.getElementById("riskDashboard");
+    console.log("Risk Dashboard Loaded");
+});
+
+// Created basic structure with a risk dashboard container and printed text to the console
